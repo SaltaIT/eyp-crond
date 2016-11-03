569d2c01e9272c6cfd4aa247f22c730984aa72b6
Jordi Prats <jprats@systemadmin.es>
2016-11-03 12:28:30 +0100

    lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,7 +7,7 @@ class crond::install inherits crond {
   if($crond::manage_package)
   {
     package { $crond::params::package_name:
-     ensure => $crond::package_ensure,
+      ensure => $crond::package_ensure,
     }
   }
 
