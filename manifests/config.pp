2d13de02f40108b87b97d252e89f4233a1968994
Jordi Prats <jprats@systemadmin.es>
2016-11-15 12:52:30 +0100

    lint

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,5 @@
-# == Class: crond
-#
-# === crond::config documentation
 #
 class crond::config inherits crond {
-
-  # han de tenir permisos 600
-  # /etc/crontab, /etc/cron.hourly, /etc/cron.weekly, /etc/cron.monthly , /etc/cron.d
-  # /etc/cron.allow ha d’existir i amb permisos 600
 
   file { $crond::params::root_cron_files:
     ensure => 'present',
