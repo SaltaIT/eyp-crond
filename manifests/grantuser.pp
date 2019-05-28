560b97764cd0af8eb556d7ae065ea88f6b6bd4c9
Jordi Prats <jordi.prats@gmail.com>
2019-05-28 16:33:02 +0200

    cleanup

--- manifests/grantuser.pp
+++ manifests/grantuser.pp
@@ -2,7 +2,6 @@ define crond::grantuser(
                           $user = $name,
                           $action = 'allow',
                         ) {
-  validate_re($action, [ '^allow$', '^deny$' ], 'Not a valid action: allow/deny')
 
   concat::fragment{ "/etc/cron.${action} ${user} ${name}":
     target  => "/etc/cron.${action}",
